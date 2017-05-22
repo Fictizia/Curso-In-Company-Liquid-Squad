9e748fe96d772425eaaa7ecb220f522630a3ce7d
Daniel Castillo <danielcastillomarfull@gmail.com>
2017-05-23 01:17:04 +0200

    listado de cursos

--- daniel-castillo/public/js/clase5.js
+++ daniel-castillo/public/js/clase5.js
@@ -6,7 +6,8 @@
 // Leer sobre bubbling de eventos
 
 
-// var teachersArray = Array.prototype.slice.call(document.getElementById('teachersList').childNodes)
+// 1 - Reorganizar la lista de profesores de Fictizia por nombre y apellidos (alfabético)
+// http://www.fictizia.com/profesores/
 var teachersArray = Array.from(document.getElementById('teachersList').childNodes)
 
 var teachersArrayOrder = teachersArray.sort(function(a, b) {
@@ -21,6 +22,42 @@ for (var i = 0; i <= teachersArrayOrder.length; i++) {
 	if(teachersArrayOrder[i] !== undefined)
 		document.getElementById('teachersList').appendChild(teachersArrayOrder[i])
 }
+
+// 2 - Saca una lista de los cursos disponibles en Fictizia en las 4 areas de formación y conviertelo en Markdown. 
+// Características:
+// Cada Bloque de cursos debe estar identificado por el título correspondiente.
+// Cada curso debe contener el enlace al mismo y especificar el número de horas entre parentesis.
+// http://www.fictizia.com/
+
+var markdown = '# Cursos de Fictizia\n\n'
+var trainingAreas = Array.from(document.getElementById('areasIndex').getElementsByTagName('li'))
+
+trainingAreas.forEach(function(area) {
+
+	var title = area.innerText.replace(/\s+/g, ' ')
+
+	markdown += '\n## ' + title + '\n'
+
+	var id = area.getAttribute('id')
+
+	id = id.substring(0, id.indexOf('_')) + '_Area'
+
+	var courses = Array.from(document.getElementById(id).getElementsByTagName('li'))
+
+	markdown += '\n**Total de cursos: ' + courses.length + '**\n'
+
+	courses.forEach(function(course) {
+		var href = '(' + course.getElementsByTagName('a')[0].getAttribute('href') + ')'
+		var hours = course.getElementsByClassName('contextualInfo')[0].innerText.match( /\d+/ig)
+		markdown += '- [' + course.getElementsByTagName('strong')[0].innerText.replace(/\s+/g, ' ') 
+					+ ' (' + hours + ' horas)]' 
+					+ href + '\n'
+	})
+
+})
+
+console.log(markdown)
+
 
 
 

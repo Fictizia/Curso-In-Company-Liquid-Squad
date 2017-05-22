e773a6c982246b98c93f61f0f10a1e07a0783999
Daniel Castillo <daniel.castillo@tecnilogica.com>
2017-05-22 19:51:00 +0200

    ordena profes

--- daniel-castillo/public/js/clase5.js
+++ daniel-castillo/public/js/clase5.js
@@ -4,3 +4,25 @@
 // http://youmightnotneedjquery.com/
 
 // Leer sobre bubbling de eventos
+
+
+var teachersArray = Array.prototype.slice.call(document.getElementById('teachersList').childNodes)
+
+var teachersArrayOrder = teachersArray.sort(function(a, b) {
+	var a = a.getElementsByTagName('img')[0].getAttribute('src')
+	var b = b.getElementsByTagName('img')[0].getAttribute('src')
+ 	return a < b ? -1 : 1
+})
+
+document.getElementById('teachersList').innerHTML = ''
+
+for (var i = 0; i <= teachersArrayOrder.length; i++) {
+
+	if(teachersArrayOrder[i] !== undefined)
+		document.getElementById('teachersList').appendChild(teachersArrayOrder[i])
+}
+
+
+
+
+

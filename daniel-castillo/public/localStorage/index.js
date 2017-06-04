4f9de0774c32c11acc7200b3e5db28e849cb9a3a
Daniel Castillo <danielcastillomarfull@gmail.com>
2017-06-04 16:31:39 +0200

    stores users array

--- daniel-castillo/public/localStorage/index.js
+++ daniel-castillo/public/localStorage/index.js
@@ -1,1 +1,18 @@
-console.log('local storage')
+saveUser = function() {
+
+
+  var users = JSON.parse(localStorage.getItem("users")) || [];
+
+  var user = {
+    name: document.getElementById('name').value,
+    phone: document.getElementById('phone').value,
+    email: document.getElementById('email').value
+  }
+
+  users.push(user)
+
+  localStorage.setItem('users', JSON.stringify(users))
+
+  console.log('users ', users)
+
+}
